3e665470664b645b9dc3c92b65b191c2f845edb8
Serhii Chekalov <78024336+Serhii-Chekalov@users.noreply.github.com>
2021-07-11 19:58:10 +0300

    task_1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import './sass/main.scss';
+const btnStart=document.querySelector('button[data-start]');
+const btnStop=document.querySelector('button[data-stop]');
+
+let timerId = null;
+const start = () => {
+    btnStart.disabled=true;
+    timerId = setInterval(() => {document.body.style.backgroundColor = getRandomHexColor()}, 1000)
+} 
+
+const stop = () => {
+    btnStart.disabled=false;
+    clearInterval(timerId)
+} 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  btnStart.addEventListener('click', start);
+  btnStop.addEventListener('click', stop); 
